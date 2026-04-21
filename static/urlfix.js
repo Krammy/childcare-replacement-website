45de7b2c1b819929e8470c9978ef4883dce1af3b
Mark Bennett <mark.bennett.dev@gmail.com>
2026-04-21 16:31:13 +0100

    updated to 'p' instead of 'page' URL param

--- static/urlfix.js
+++ static/urlfix.js
@@ -1,5 +1,5 @@
 const params = new URLSearchParams(window.location.search);
-const page = params.get('page');
+const page = params.get('p');
 
 if (page != null) {
     const new_url = window.location.protocol + "//" + window.location.host + "/" + page;
